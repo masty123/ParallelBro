012c2c419ccac2180ca7c3d1f6d1ff9e18e2ed67
masty123 <theeruthborisuth@kamk.fi>
2019-09-10 12:18:49 +0300

    add timer and pixel ui

---
+++ Assets/Scripts/ScrGame/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    [SerializeField] float startTime;
+
+    private void Update()
+    {
+        float t = startTime - Time.time;
+        if (t >= 0)
+        {
+            string minutes = ((int)t / 60).ToString();
+            string seconds = (t % 60).ToString("f2");
+            timerText.text = minutes + ":" + seconds;
+        }
+        else
+        {
+            // Application.Quit();
+            Debug.Log("Game Over");
+        }
+    }
+}
