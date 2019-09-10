a0ac5b4618f7d19d081d02ec05149a74b8b10496
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-09-10 14:44:12 +0300

    Added scene to test switch implementation on changing property of the box when turned on

---
+++ Assets/Scripts/ScrEnvironment/Switch/Switches/PrinttoTestStageComponents/TestingSwitchPrintto01.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingSwitchPrintto01 : ITurningSwitch
+{
+
+    public GameObject BoxToChangeColor;
+
+    Animator anim;
+    Animator boxAnim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        if (IsOn)
+        {
+            anim.SetTrigger("TurnOn");
+        }
+        else
+        {
+            anim.SetTrigger("TurnOff");
+        }
+        boxAnim = BoxToChangeColor.GetComponent<Animator>();
+    }
+
+    public override bool turn()
+    {
+        IsOn = true;
+        if (IsOn)
+        {
+            anim.SetTrigger("TurnOn");
+            boxAnim.SetTrigger("Active");
+        }
+        else
+        {
+            //anim.SetTrigger("TurnOff");
+        }
+        return IsOn;
+    }
+}
