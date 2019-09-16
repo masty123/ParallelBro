cdb43c5ffb0e447b2e6b4ab6c032901b901a8777
Thitiwat Thongbor <thitiwat.thongbor@gmail.com>
2019-09-16 11:08:51 +0300

    Add Interactable

---
+++ Assets/Scripts/ScrEnvironment/BaseEnvironment/IInteractable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public enum EffectType
+{
+    EFFECT_BOTH,
+    EFFECT_OWN,
+    EFFECT_OTHER
+}
+
+public abstract class IInteractable : MonoBehaviourPun
+{
+    private int ID;
+    public EffectType effectType = EffectType.EFFECT_BOTH;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // assign ID;
+        ID = InteractableFactory.Instance.GetID();
+    }
+
+    public abstract void Interact();
+}
---
+++ Assets/Scripts/ScrEnvironment/BaseEnvironment/InteractableFactory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableFactory : MonoBehaviour
+{
+    public static InteractableFactory Instance;
+    private Dictionary<int, IInteractable> storage;
+    private int lastID;
+
+    void Awake()
+    {
+        if (InteractableFactory.Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+        storage = new Dictionary<int, IInteractable>();
+        lastID = 1000;
+    }
+
+    public int GetID()
+    {
+        return lastID++;
+    }
+
+    public IInteractable GetInteractable(int ID)
+    {
+        if(storage[ID] == null)
+        {
+            Debug.LogWarning("GetInteractable : " + ID + " could not be found..");
+        }
+        return storage[ID];
+    }
+}
