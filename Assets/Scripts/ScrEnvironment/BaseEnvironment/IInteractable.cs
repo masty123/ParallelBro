adebf279351a21179bb2cc79eea7fc0698c9b5e1
Thitiwat Thongbor <thitiwat.thongbor@gmail.com>
2019-09-16 11:15:23 +0300

    Add Notify network

--- Assets/Scripts/ScrEnvironment/BaseEnvironment/IInteractable.cs
+++ Assets/Scripts/ScrEnvironment/BaseEnvironment/IInteractable.cs
@@ -23,4 +23,27 @@ public abstract class IInteractable : MonoBehaviourPun
     }
 
     public abstract void Interact();
+    [PunRPC]
+    public void Interact(int ID)
+    {
+        Debug.Log("Interact ID : " + ID);
+        InteractableFactory.Instance.GetInteractable(ID).Interact();
+    }
+
+    public void NotifyNetwork(PhotonView photonView)
+    {
+        if (photonView == null)
+        {
+            Debug.LogWarning("You have called NotifyNetwork in Offline instance");
+            Interact();
+            return;
+        }
+
+        switch (effectType)
+        {
+            case EffectType.EFFECT_BOTH: photonView.RPC("Interact", RpcTarget.AllBuffered, ID); break;
+            case EffectType.EFFECT_OWN: Interact(ID); break;
+            case EffectType.EFFECT_OTHER: photonView.RPC("Interact", RpcTarget.OthersBuffered, ID); break;
+        }
+    }
 }
