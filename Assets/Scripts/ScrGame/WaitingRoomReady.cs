be59925dc64d198b8ee2a626b9f860a3cf9df584
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-10-08 14:35:00 +0300

    Added waiting room script

---
+++ Assets/Scripts/ScrGame/WaitingRoomReady.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitingRoomReady : MonoBehaviour
+{
+
+    bool isReady = false;
+
+    public void readyPressed()
+    {
+        if (!isReady)
+        {
+            isReady = true;
+            //TODO: Add ready player
+            //TODO: If ready player == 2 : start the scene?
+        }
+        else {
+            isReady = false;
+            //TODO: Remove from ready players
+        }
+    }
+
+}
