890c780d47aa3ce149fb97b30ac723b68bca543c
Thongbor Thitiwat TIX18 <thitiwatthongbor@kamk.fi>
2019-10-29 10:24:03 +0200

    add hide able platform

---
+++ Assets/Scripts/ScrEnvironment/Platform/HideAblePlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideAblePlatform : ITargetable
+{
+    public SpriteRenderer sprite;
+    public Collider2D _collider;
+
+    public bool isOn = false;
+    public bool IsToggle = true;
+
+
+    public override void Turn()
+    {
+        if(isOn && !IsToggle)
+        {
+            return;
+        }
+
+        isOn = !isOn;
+        Toggle(this.isOn);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Toggle(this.isOn);
+    }
+
+    private void Toggle(bool isOn)
+    {
+        sprite.color = isOn ? Color.white : Color.black;
+        _collider.enabled = isOn;
+    }
+}
---
+++ Assets/Scripts/ScrEnvironment/Platform/HideOtherOnStepPlatform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOtherOnStepPlatform : MonoBehaviour
+{
+    public Switch _switch;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if character
+        if(collision.gameObject.tag == "player")
+        {
+            // sent other RPC to toggle switch
+            _switch.Interact();
+        }
+    }
+}
