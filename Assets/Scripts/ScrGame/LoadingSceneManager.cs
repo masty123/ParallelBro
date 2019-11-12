a06b976a08602ab9459d9bdb0bd35b0807736520
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-11-12 13:39:28 +0200

    Added loading scene

---
+++ Assets/Scripts/ScrGame/LoadingSceneManager.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingSceneManager : MonoBehaviour
+{
+
+    public float delaySeconds = 2f;
+
+    public Text loadingText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadText();
+        StartCoroutine(LoadSceneWithDelaySeconds());
+    }
+
+    IEnumerator LoadSceneWithDelaySeconds()
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        PhotonNetwork.LoadLevel("level" + RoomData.GetInstance().level);
+    }
+
+    void loadText()
+    {
+        if(loadingText != null)
+        {
+            loadingText.text += "Level" + RoomData.GetInstance().level;
+        }
+    }
+
+}
