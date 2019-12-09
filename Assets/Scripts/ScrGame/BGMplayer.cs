59e2eb883a9c289030970d94a038bcc9ff9d5110
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-12-09 16:58:41 +0200

    BGM?

---
+++ Assets/Scripts/ScrGame/BGMplayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMplayer : MonoBehaviour
+{
+
+    static BGMplayer instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+}
