dbed2961b1e46848f0c782ea6dfee320c00957a7
Thitiwat Thongbor <thitiwat.thongbor@gmail.com>
2019-12-09 15:24:48 +0200

    Fix background

---
+++ Assets/Scripts/ScrGame/Background.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int index = UserData.GetInstance().GetCharacterIndex();
+        if (index == 2)
+        {
+            transform.localRotation = Quaternion.Euler(180, 0, 0);
+        }
+    }
+}
--- Assets/Scripts/ScrGame/Parallax.cs
+++ Assets/Scripts/ScrGame/Parallax.cs
@@ -8,6 +8,8 @@ public class Parallax : MonoBehaviour
     public GameObject cam;
     public float parallaxEffect;
 
+    public bool isFolllowVertical = false;
+
     void Start()
     {
         startpos = transform.position.x;
@@ -18,6 +20,9 @@ public class Parallax : MonoBehaviour
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
+
+        if (isFolllowVertical)
+            this.transform.position = new Vector3(transform.position.x, cam.transform.position.y, transform.position.z);
 
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
         if (temp > startpos + length) startpos += length;
